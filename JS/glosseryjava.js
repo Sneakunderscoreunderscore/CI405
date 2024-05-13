274a33d43b447d9532c77f6e8388921010cce6bf
Magic4lPug <159052210+Magic4lPug@users.noreply.github.com>
2024-05-13 21:36:03 +0200

    fixed glosseryjava.js

--- JS/glosseryjava.js
+++ JS/glosseryjava.js
@@ -2,14 +2,12 @@ document.getElementById('searchInput').addEventListener('input', function() {
   var searchQuery = this.value.toLowerCase();
   var glossaryItems = document.querySelectorAll('#glossaryList li');
 
-    glossaryItems.forEach(function(item) {
-        if (item.textContent.toLowerCase().includes(searchQuery)) {
-        item.style.display = '';
-        }          
-        else {
-        item.style.display = 'none';
-        }
-      
-    });
-    
+  glossaryItems.forEach(function(item) {
+    var term = item.querySelector('b');
+    if (term && term.textContent.toLowerCase().includes(searchQuery)) {
+      item.style.display = ''; 
+    } else {
+      item.style.display = 'none'; 
+    }
+  });
 });
