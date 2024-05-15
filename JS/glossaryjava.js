489e47428f7171bd47798a87225f1dca54e462c5
Magic4lPug <159052210+Magic4lPug@users.noreply.github.com>
2024-05-15 18:40:55 +0200

    Update glossaryjava.js

--- JS/glossaryjava.js
+++ JS/glossaryjava.js
@@ -1,13 +1,15 @@
-document.getElementById('searchInput').addEventListener('input', function() {
-  var searchQuery = this.value.toLowerCase();
-  var glossaryItems = document.querySelectorAll('#glossaryList li');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchInput').addEventListener('input', function() {
+        var searchQuery = this.value.toLowerCase();
+        var glossaryItems = document.querySelectorAll('#glossaryList li');
 
-  glossaryItems.forEach(function(item) {
-    var term = item.querySelector('b');
-    if (term && term.textContent.toLowerCase().includes(searchQuery)) {
-      item.style.display = ''; 
-    } else {
-      item.style.display = 'none'; 
-    }
-  });
+        glossaryItems.forEach(function(item) {
+            var term = item.querySelector('b');
+            if (term && term.textContent.toLowerCase().includes(searchQuery)) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
 });
